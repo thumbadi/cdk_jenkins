1dca6d7d197cac2d38f20016b94e3ba817b5dbd8
EC2 Default User <ec2-user@ip-172-31-15-251.us-west-2.compute.internal>
2023-01-27 14:56:42 +0000

    second commit

--- src/main/java/com/myorg/conf.java
+++ src/main/java/com/myorg/conf.java
@@ -10,10 +10,11 @@ public class conf {
     public HashMap<String,Object> iam_group3 = new HashMap<String,Object>();
     public HashMap<String,Object> iam_group4 = new HashMap<String,Object>();
     public HashMap<String,Object> iam_group5 = new HashMap<String,Object>();
+    public HashMap<String,Object> iam_group6 = new HashMap<String,Object>();
     public ArrayList<HashMap<String,Object>> groups = new ArrayList<HashMap<String,Object>>();
     
     // list of users you want to create
-    List<String> users = List.of("user1","user2","user3","user6","user7");
+    List<String> users = List.of("user1","user2","user3","user6","user7","user8","user9","user10");
     
     // list of groups you want to create
     public ArrayList<HashMap<String,Object>> groupsList(){
@@ -45,6 +46,10 @@ public class conf {
         this.iam_group5.put("groupname","group5");
         this.iam_group5.put("users", List.of("user1","user2","user3"));
         this.iam_group5.put("actions", List.of("iam:*","eks:*"));
+        
+        this.iam_group6.put("groupname","group6");
+        this.iam_group6.put("users", List.of("user8","user9","user10"));
+        this.iam_group6.put("actions", List.of("iam:*","ecs:*"));
        
         
         this.groups.add(this.iam_group1);
@@ -52,6 +57,7 @@ public class conf {
         this.groups.add(this.iam_group3);
         this.groups.add(this.iam_group4);
         this.groups.add(this.iam_group5);
+        this.groups.add(this.iam_group6);
         
         return this.groups;
     }

04ed020a36f4412fff407ec3128f94b9a6bcc4f9
thumbadi <girishmca@gmail.com>
2025-02-17 14:04:58 -0500

    Create MessageContent.java

---
+++ MessageContent.java
@@ -0,0 +1,200 @@
+package com.example.awssqsdemo;
+
+public class MessageContent {
+	private String data_src_cd;
+    private String rec_type;
+    private String pde_rcvd_dt;
+    private String pde_rcvd_tm;
+    private String pkg_audt_key_id;
+    private String adjstmt_dltn_cd;
+	private String dos;
+    private String pres_srvc_ref_no;
+    private String fill_num;
+    private String srvc_prov_id_qlfr;
+    private String srvc_prov_id;
+    private String alt_srvc_prov_id_qlfr;
+    private String alt_srvc_prov_id;
+    private String phrmcy_srvc_type_cd;
+    private String prscrbr_qlfr;
+    private String prscrbr_id;
+    private String prod_srvc_id;
+    private String qty_dspnsd;
+    private String days_suply;
+    private String ind_340b;
+    private String sub_cntrct;
+    private String clm_cntl_num;
+    private String cardhldr_id;
+
+    private String received_id;
+    private String received_dt;
+    private String internal_claim_num;
+    private String mtf_curr_claim_stus_ref_cd;
+    
+    
+    
+    
+    public String getFill_num() {
+		return fill_num;
+	}
+	public void setFill_num(String fill_num) {
+		this.fill_num = fill_num;
+	}
+	public String getReceived_id() {
+		return received_id;
+	}
+	public void setReceived_id(String received_id) {
+		this.received_id = received_id;
+	}
+	public String getReceived_dt() {
+		return received_dt;
+	}
+	public void setReceived_dt(String received_dt) {
+		this.received_dt = received_dt;
+	}
+	public String getInternal_claim_num() {
+		return internal_claim_num;
+	}
+	public void setInternal_claim_num(String internal_claim_num) {
+		this.internal_claim_num = internal_claim_num;
+	}
+	public String getMtf_curr_claim_stus_ref_cd() {
+		return mtf_curr_claim_stus_ref_cd;
+	}
+	public void setMtf_curr_claim_stus_ref_cd(String mtf_curr_claim_stus_ref_cd) {
+		this.mtf_curr_claim_stus_ref_cd = mtf_curr_claim_stus_ref_cd;
+	}
+	public String getData_src_cd() {
+		return data_src_cd;
+	}
+	public void setData_src_cd(String data_src_cd) {
+		this.data_src_cd = data_src_cd;
+	}
+	public String getRec_type() {
+		return rec_type;
+	}
+	public void setRec_type(String rec_type) {
+		this.rec_type = rec_type;
+	}
+	public String getPde_rcvd_dt() {
+		return pde_rcvd_dt;
+	}
+	public void setPde_rcvd_dt(String pde_rcvd_dt) {
+		this.pde_rcvd_dt = pde_rcvd_dt;
+	}
+	public String getPde_rcvd_tm() {
+		return pde_rcvd_tm;
+	}
+	public void setPde_rcvd_tm(String pde_rcvd_tm) {
+		this.pde_rcvd_tm = pde_rcvd_tm;
+	}
+	public String getPkg_audt_key_id() {
+		return pkg_audt_key_id;
+	}
+	public void setPkg_audt_key_id(String pkg_audt_key_id) {
+		this.pkg_audt_key_id = pkg_audt_key_id;
+	}
+	public String getAdjstmt_dltn_cd() {
+		return adjstmt_dltn_cd;
+	}
+	public void setAdjstmt_dltn_cd(String adjstmt_dltn_cd) {
+		this.adjstmt_dltn_cd = adjstmt_dltn_cd;
+	}
+	public String getDos() {
+		return dos;
+	}
+	public void setDos(String dos) {
+		this.dos = dos;
+	}
+	public String getPres_srvc_ref_no() {
+		return pres_srvc_ref_no;
+	}
+	public void setPres_srvc_ref_no(String pres_srvc_ref_no) {
+		this.pres_srvc_ref_no = pres_srvc_ref_no;
+	}
+	public String getSrvc_prov_id_qlfr() {
+		return srvc_prov_id_qlfr;
+	}
+	public void setSrvc_prov_id_qlfr(String srvc_prov_id_qlfr) {
+		this.srvc_prov_id_qlfr = srvc_prov_id_qlfr;
+	}
+	public String getSrvc_prov_id() {
+		return srvc_prov_id;
+	}
+	public void setSrvc_prov_id(String srvc_prov_id) {
+		this.srvc_prov_id = srvc_prov_id;
+	}
+	public String getAlt_srvc_prov_id_qlfr() {
+		return alt_srvc_prov_id_qlfr;
+	}
+	public void setAlt_srvc_prov_id_qlfr(String alt_srvc_prov_id_qlfr) {
+		this.alt_srvc_prov_id_qlfr = alt_srvc_prov_id_qlfr;
+	}
+	public String getAlt_srvc_prov_id() {
+		return alt_srvc_prov_id;
+	}
+	public void setAlt_srvc_prov_id(String alt_srvc_prov_id) {
+		this.alt_srvc_prov_id = alt_srvc_prov_id;
+	}
+	public String getPhrmcy_srvc_type_cd() {
+		return phrmcy_srvc_type_cd;
+	}
+	public void setPhrmcy_srvc_type_cd(String phrmcy_srvc_type_cd) {
+		this.phrmcy_srvc_type_cd = phrmcy_srvc_type_cd;
+	}
+	public String getPrscrbr_qlfr() {
+		return prscrbr_qlfr;
+	}
+	public void setPrscrbr_qlfr(String prscrbr_qlfr) {
+		this.prscrbr_qlfr = prscrbr_qlfr;
+	}
+	public String getPrscrbr_id() {
+		return prscrbr_id;
+	}
+	public void setPrscrbr_id(String prscrbr_id) {
+		this.prscrbr_id = prscrbr_id;
+	}
+	public String getProd_srvc_id() {
+		return prod_srvc_id;
+	}
+	public void setProd_srvc_id(String prod_srvc_id) {
+		this.prod_srvc_id = prod_srvc_id;
+	}
+	public String getQty_dspnsd() {
+		return qty_dspnsd;
+	}
+	public void setQty_dspnsd(String qty_dspnsd) {
+		this.qty_dspnsd = qty_dspnsd;
+	}
+	public String getDays_suply() {
+		return days_suply;
+	}
+	public void setDays_suply(String days_suply) {
+		this.days_suply = days_suply;
+	}
+	public String getInd_340b() {
+		return ind_340b;
+	}
+	public void setInd_340b(String ind_340b) {
+		this.ind_340b = ind_340b;
+	}
+	public String getSub_cntrct() {
+		return sub_cntrct;
+	}
+	public void setSub_cntrct(String sub_cntrct) {
+		this.sub_cntrct = sub_cntrct;
+	}
+	public String getClm_cntl_num() {
+		return clm_cntl_num;
+	}
+	public void setClm_cntl_num(String clm_cntl_num) {
+		this.clm_cntl_num = clm_cntl_num;
+	}
+	public String getCardhldr_id() {
+		return cardhldr_id;
+	}
+	public void setCardhldr_id(String cardhldr_id) {
+		this.cardhldr_id = cardhldr_id;
+	}
+	
+
+}

e72588b7e1d66e5e4bbea17a57e318e3e00837e1
thumbadi <girishmca@gmail.com>
2025-03-28 09:11:13 -0400

    Update pyspark-parquet to s3.py

--- pyspark-parquet to s3.py
+++ pyspark-parquet to s3.py
@@ -33,7 +33,7 @@ job.init(args['JOB_NAME'], args)
 def postgres_query(db,schema,table,**kwargs):    
     seq = kwargs.get("action")
     if seq == "read":
-        query = f"""(select c.internal_claim_num as "MTF_ICN", c.xref_internal_claim_num as "MTF_XREF_ICN", coalesce(c.mrn_process_dt, CURRENT_DATE) as "PROCESS_DT",
+        query = f"""(select c.internal_claim_num as "MTF_ICN", c.xref_internal_claim_num as "MTF_XREF_ICN", c.received_dt as "RECEIVED_DT", coalesce(c.mrn_process_dt, CURRENT_DATE) as "PROCESS_DT",
 c.src_claim_type_cd as "SRC_CLAIM_TYPE_CODE", c.medicare_src_of_coverage as "MEDICARE_SRC_OF_COVERAGE", c.srvc_dt as "SRVC_DT", 
 c.rx_srvc_ref_num as "RX_SRVC_REF_NUM", coalesce(c.fill_num,'0') as "FILL_NUM", c.ncpdp_id as "NCPDP_ID", c.srvc_npi_num as "SRVC_PRVDR_ID", 
 c.prescriber_id as "PRESCRIBER_ID", c.ndc_cd as "NDC_CD", c.quantity_dispensed as "QUANTITY_DISPENSED", c.days_supply  as "DAYS_SUPPLY", 
@@ -71,7 +71,7 @@ where c.mtf_curr_claim_stus_ref_cd = 'MRN')
         
         query = """
             UPDATE claim.mtf_claim \
-            SET mtf_curr_claim_stus_ref_cd = 'RCD', update_ts = %s, update_user_id = %s\
+            SET mtf_curr_claim_stus_ref_cd = 'RCD', update_ts = %s, update_user_id = %s, mrn_process_dt = %s\
             WHERE internal_claim_num = (%s) AND received_dt = %s
         """       
              
@@ -229,9 +229,9 @@ for entry in schema_data:
             elif key == "update":
                 df_final["update_ts"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
                 df_final["update_user_id"] = -1
-                #df_final['received_dt']= df_final['received_dt'].apply(lambda x: pd.to_datetime(x).strftime('%Y-%m-%d'))
+                df_final['received_dt']= df_final['received_dt'].apply(lambda x: pd.to_datetime(x).strftime('%Y-%m-%d'))
                 df_final['process_dt']= df_final['process_dt'].apply(lambda x: pd.to_datetime(x).strftime('%Y-%m-%d'))
-                claim_id = df_final[['update_ts','update_user_id','mtf_icn','process_dt']].values.tolist()
+                claim_id = df_final[['update_ts','update_user_id','mtf_icn','mrn_process_dt','received_dt']].values.tolist()
                 postgres_query(database,schema,table,id = claim_id, action="update")
             elif key == "insert":
                 postgres_query(database,schema,table,action="insert", dat=df_final)
